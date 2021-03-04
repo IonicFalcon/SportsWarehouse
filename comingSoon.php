f3d603b788eab72e51a6bfd522d8424f9375134e
IonicFalcon <joshuajwallen@gmail.com>
2021-03-04 12:32:03 +1100

    Added Styling

--- comingSoon.php
+++ comingSoon.php
@@ -15,6 +15,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coming Soon - SportsWarehouse</title>
+    <link rel="stylesheet" href="css/comingSoon.css">
 </head>
 <body>
     <div class="root">
@@ -26,26 +27,29 @@
             <p>SportsWarehouse is coming soon. If you have any questions, we would love to hear from you, please complete the following information.</p>
 
             <form action="controllers/comingSoonControl.php" name="feedback" method="post" onsubmit="return ValidateForm()">
-                <p>
-                    <label for="firstName" <?= in_array("firstName", $errorList) ? 'class="error"' : '' ?> >First Name *</label>
-                    <input type="text" id="firstName" name="firstName" required value="<?= $filledFields["firstName"] ?? "" ?>">
-                </p>
-                <p>
-                    <label for="lastName" <?= in_array("lastName", $errorList) ? 'class="error"' : '' ?>>Last Name *</label>
-                    <input type="text" id="lastName" name="lastName" required value="<?= $filledFields["lastName"] ?? "" ?>">
-                </p>
-                <p>
-                    <label for="contactNumber">Contact Number</label>
-                    <input type="tel" name="contactNumber" id="contactNumber" value="<?= $filledFields["contactNumber"] ?? "" ?>">
-                </p>
-                <p>
-                    <label for="email" <?= in_array("email", $errorList) ? 'class="error"' : '' ?>>Email *</label>
-                    <input type="email" name="email" id="email" required value="<?= $filledFields["email"] ?? "" ?>">
-                </p>
-                <p>
-                    <label for="question" <?= in_array("question", $errorList) ? 'class="error"' : '' ?>>Question *</label>
-                    <textarea name="question" id="question" cols="30" rows="10" required><?= $filledFields["question"] ?? "" ?></textarea>
-                </p>
+                <fieldset>
+                    <p class="formInput">
+                        <input type="text" id="firstName" name="firstName" required value="<?= $filledFields["firstName"] ?? "" ?>">
+                        <label for="firstName" <?= in_array("firstName", $errorList) ? 'class="error"' : '' ?> >First Name</label>
+                    </p>
+                    <p class="formInput">
+                        <input type="text" id="lastName" name="lastName" required value="<?= $filledFields["lastName"] ?? "" ?>">
+                        <label for="lastName" <?= in_array("lastName", $errorList) ? 'class="error"' : '' ?>>Last Name</label>
+                    </p>
+                    <p class="formInput">
+                        <input type="tel" name="contactNumber" id="contactNumber" value="<?= $filledFields["contactNumber"] ?? "" ?>">
+                        <label for="contactNumber">Contact Number</label>
+                    </p>
+                    <p class="formInput">
+                        <input type="email" name="email" id="email" required value="<?= $filledFields["email"] ?? "" ?>">
+                        <label for="email" <?= in_array("email", $errorList) ? 'class="error"' : '' ?>>Email</label>
+                    </p>
+                    <p class="formInput">
+                        <textarea name="question" id="question" cols="30" rows="5" required><?= $filledFields["question"] ?? "" ?></textarea>
+                        <label for="question" <?= in_array("question", $errorList) ? 'class="error"' : '' ?>>Question</label>
+                    </p>
+                </fieldset>
+                
 
                 <p id="errorMessage">
                     <?php
