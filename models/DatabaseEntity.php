286c7736ac95e9e1d689f84e6dbafd37ad17dc5f
IonicFalcon <joshuajwallen@gmail.com>
2021-03-31 22:31:12 +1100

    Encorporated DB Settings into DatabaseEntity class

--- models/DatabaseEntity.php
+++ models/DatabaseEntity.php
@@ -6,7 +6,18 @@
 abstract class DatabaseEntity{
     protected static function DB(){
         include_once "Database.php";
-        include "settings/DBSettings.php";
+        
+        if($_SERVER["SERVER_NAME"] == "localhost" || $_SERVER["SERVER_ADDR" == "127.0.0.1"]){
+            $serverName = "localhost";
+            $username = "root";
+            $password = "";
+            $dbName = "SportsWarehouse";
+        } else{
+            $serverName = "localhost";
+            $username = "magenta02";
+            $password = "father71";
+            $dbName = "magenta02";
+        }
 
         return new Database($serverName, $username, $password, $dbName);
     }
