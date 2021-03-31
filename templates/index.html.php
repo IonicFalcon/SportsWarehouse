168a45fe22267b79e28867fd7db6800ec2d34032
Joshua Allen <Joshua.Allen5@studytafensw.edu.au>
2021-03-31 15:56:47 +1100

    Added Template Files

---
+++ templates/index.html.php
@@ -0,0 +1,82 @@
+<div class="slider">
+    <div id="slideBall">
+        <div class="slideDescription">
+            <p>View our brand new range of</p>
+            <h2>Sports Balls</h2>
+            <a href="searchProducts.php?cat=5" class="linkButton">Shop Now</a>
+        </div>
+    </div>
+    <div id="slideArmour">
+        <div class="slideDescription">
+            <p>Get protected with the new range of</p>
+            <h2>Protective Helmets</h2>
+            <a href="searchProducts.php?cat=2" class="linkButton">Shop Now</a>
+        </div>
+    </div>
+    <div id="slideRace">
+        <div class="slideDescription">
+            <p>Get ready to race with our professional</p>
+            <h2>Training Gear</h2>
+            <a href="searchProducts.php?cat=7" class="linkButton">Shop Now</a>
+        </div>
+    </div>
+</div>
+<section class="featuredProducts">
+    <h2>Featured Products</h2>
+    <ul>
+        <?php
+            foreach($featuredItems as $item){
+                ?>
+                    <li>
+
+                        <a href="showProduct.php?id=<?= $item->ItemID ?>" class="featuredProduct">
+                            <article class="productCard">
+                                <div class="productImage">
+                                    <img src="images/productImages/<?= $item->Photo ?>" alt="<?= $item->ItemName?> Image">
+                                </div>
+    
+                                <?php
+                                    if(isset($item->SalePrice) && floatval($item->SalePrice) > 0){
+                                        ?>
+                                            <div class="productPrice productSale">
+                                                <span>$<?= $item->SalePrice ?></span>
+                                                <p>was <s>$<?= $item->Price ?></s></p>
+                                            </div>
+                                        <?php
+                                    } else{
+                                        ?>
+                                            <div class="productPrice">
+                                                <p>$<?= $item->Price ?></p>
+                                            </div>
+                                        <?php
+                                    }
+                                ?>
+                                
+                                <h3 class="productName"><?= $item->ItemName ?></h3>
+                            </article>
+                        </a>
+                    </li>
+                <?php
+            }
+        ?>
+    </ul>
+</section>
+<section class="brandsAndPartnerships">
+    <h2>Our Brands and Partnerships</h2>
+
+    <div class="brandContent">
+        <div class="brandDescription">
+            <p>These are some of our top brands and partnerships.</p> 
+            <p><span>The best of the best is here.</span></p>
+        </div>
+
+        <ul class="partnerships">
+            <li><img src="images/partnershipLogos/logo_nike.png" alt="Nike Logo"></li>
+            <li><img src="images/partnershipLogos/logo_adidas.png" alt="Adidas Logo"></li>
+            <li><img src="images/partnershipLogos/logo_skins.png" alt="Skins Logo"></li>
+            <li><img src="images/partnershipLogos/logo_asics.png" alt="Asics Logo"></li>
+            <li><img src="images/partnershipLogos/logo_newbalance.png" alt="New Balance Logo"></li>
+            <li><img src="images/partnershipLogos/logo_wilson.png" alt="Wilson Logo"></li>
+        </ul>
+    </div> 
+</section>
---
+++ templates/layout.html.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $pageTitle ?></title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/0b7408c7cc.js" crossorigin="anonymous"></script>
+
+    <?php
+        //Import any other relevant files for the specific page
+        if(isset($CSSSources)){
+            foreach($CSSSources as $file){
+                ?>
+                    <link rel="stylesheet" href="<?= $file ?>">
+                <?php
+            }
+        }
+    ?>
+
+</head>
+<body>
+    <div class="root">
+        <header class="siteHeader">
+            <div class="topBar">
+                <div class="wrapper">
+                    <div class="hamburgerMenu">
+                        <input type="checkbox" name="mobileHamburger" id="mobileHamburger" onchange="mobileMenu(this)">
+                        <label for="mobileHamburger" class="iconButton hamburgerIcon">Menu</label>
+                    </div>
+    
+                    <nav class="siteNav desktopNav">
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="#">About SW</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                            <li><a href="searchProducts.php">View Products</a></li>
+                        </ul>               
+                    </nav>
+    
+                    <div class="userOptions">
+                        <a href="#" class="login iconButton">Login</a>
+                        <a href="#" class="viewCart iconButton">
+                            <p>View Cart</p>
+                            <span class="cartTotal">0 Items</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <nav class="mobileNav siteNav">
+                <ul>
+                    <li><a href="#" class="login iconButton">Login</a></li>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="#">About SW</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="searchProducts.php">View Products</a></li>
+                </ul>  
+            </nav>
+
+            <div class="siteBanner wrapper">
+                <a href="index.php" class="siteLogo" aria-label="Sports Warehouse">
+                    <img src="images/LogoLarge.gif" alt="Sports Warehouse Logo">
+                </a>
+            
+                <form method="get" class="search">
+                    <input type="text" name="search" id="searchBox" aria-label="Search Products" placeholder="Search Products">
+                    <button type="submit" class="searchButton" aria-label="Search">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </form>
+            </div>
+            <div class="categoryBar wrapper">
+                <nav class="categoryNav">
+                    <ul>
+                        <?php
+                            foreach($categories as $category){
+                                ?>
+                                    <li><a href="searchProducts.php?cat=<?= $category->CategoryID ?>"><?= $category->CategoryName ?></a></li>
+                                <?php
+                            }
+                        ?>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <main class="wrapper">
+            <?= $mainOutput ?>
+        </main>
+        <footer class="siteFooter">
+        <div class="wrapper">
+                <div class="footerGroups">
+                    <div class="footerSiteNav">
+                        <nav class="siteNav">
+                            <h2>Site Navigation</h2>
+                            <ul>
+                                <li><a href="index.php">Home</a></li>
+                                <li><a href="#">About SW</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                                <li><a href="searchProducts.php">View Products</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="footerProductCategories">
+                        <nav class="categoryNav">
+                            <h2>Product Categories</h2>
+                            <ul>
+                                <?php
+                                    foreach($categories as $category){
+                                        ?>
+                                            <li><a href="searchProducts.php?cat=<?= $category->CategoryID ?>"><?= $category->CategoryName ?></a></li>
+                                        <?php
+                                    }
+                                ?>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="footerContact">
+                        <h2>Contact Sports Warehouse</h2>
+                        <ul>
+                            <li>
+                                <a href="#" id="contactFacebook" class="contactIcon">
+                                    <div class="iconCircle">
+                                        <i class="fab fa-facebook-f"></i>
+                                    </div>
+                                    <p>Facebook</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" id="contactTwitter" class="contactIcon">
+                                    <div class="iconCircle">
+                                        <i class="fab fa-twitter"></i>
+                                    </div>
+                                    <p>Twitter</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" id="contactOther" class="contactIcon">
+                                    <div class="iconCircle">
+                                        <i class="fas fa-paper-plane"></i>
+                                    </div>
+                                    <p>Other</p>
+                                    <ul>
+                                        <li>Online Form</li>
+                                        <li>Email</li>
+                                        <li>Phone</li>
+                                        <li>Address</li>
+                                    </ul>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="copyrightDetails">
+                <small>
+                    <p>&copy; Copyright 2020 Sports Warehouse.</p>
+                    <p>All rights reserved.</p>
+                    <p>Website made by Awesomesauce Design</p>
+                </small>
+            </div>
+        </footer>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="js/layout.js"></script>
+    <?php
+        //Import any other relevant files for the specific page
+        if(isset($JSSources)){
+            foreach($JSSources as $file){
+                ?>
+                    <script src="<?=$file?>"></script>
+                <?php
+            }
+        }
+    ?>
+</body>
+</html>
