038240ed96fcb82626f62b258013ba5ece4054e8
IonicFalcon <joshuajwallen@gmail.com>
2021-04-01 13:41:35 +1100

    Fixed AJAX issues

--- js/showProduct.js
+++ js/showProduct.js
@@ -1,26 +1,21 @@
-import {SubmitAJAXRequest} from "./modules/AJAX.js";
-
 $("#addToCart").submit(function(event) {
     event.preventDefault();
 
     let url = $(this).attr("action");
     let formData = new FormData(this);
 
-    let returnData = SubmitAJAXRequest(url, formData);
-    let shoppingCart = document.querySelector(".cartTotal");
-
-    shoppingCart.innerText = returnData.CartItems;
-})
-
-$(document).ready(()=>{
-    if(screen.width >= 450){
-        let zoomOptions = {
-            width: 500,
-            zoomWidth: 1000,
-            zoomContainer: document.querySelector(".zoomContainer"),
-            zoomStyle: "z-index: 2;position: absolute;"
-        };
-
-        new ImageZoom(document.querySelector(".zoomContainer"), zoomOptions);
-    }
-})
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: formData,
+        success: (returnData) =>{
+            returnData = JSON.parse(returnData);
+            
+            let shoppingCart = document.querySelector(".cartTotal");
+            shoppingCart.innerHTML = returnData.CartItems;
+        },
+        datatype: "json",
+        processData: false,
+        contentType: false
+    });
+});
