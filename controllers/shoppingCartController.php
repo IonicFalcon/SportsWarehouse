8e25c28fadd07ef7dec19b0bf1a0072e50dd8e0e
IonicFalcon <joshuajwallen@gmail.com>
2021-04-01 12:31:15 +1100

    Fixed AJAX Issue

--- controllers/shoppingCartController.php
+++ controllers/shoppingCartController.php
@@ -1,24 +1,29 @@
 <?php
 
+//No user should be able to see this page; if not POST, redirect them back to the homepage
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     session_start();
     include "../models/Item.php";
+    include "../models/ShoppingCart.php";
 
+    //Check if a shopping cart session already exist; if not, create a new one
     if(isset($_SESSION["ShoppingCart"])){
         $shoppingCart = unserialize($_SESSION["ShoppingCart"]);
     } else{
-        include "../models/ShoppingCart.php";
         $shoppingCart = new ShoppingCart();
     }
 
+    //Store Item data and quantity in Item object
     $item = Item::GetItemFromID($_POST["itemID"]);
     $item->Quantity = $_POST["quantity"];
 
-    array_push($shoppingCart->Items, $item);
+    //Add Item to Shopping Cart and put Shopping Cart object back in Session
+    $shoppingCart->AddItem($item);
     $_SESSION["ShoppingCart"] = serialize($shoppingCart);
 
-    $returnData = ["ShoppingCartLength" => sizeof($shoppingCart->Items)];
-    json_encode($returnData);
+    //Format and echo data to be returned as JSON
+    $returnData = ["CartItems" => $shoppingCart->ItemCount()];
+    echo json_encode($returnData);
 } else{
     header("Location: ../index.php");
     die();
--- js/showProduct.js
+++ js/showProduct.js
@@ -1,20 +1,26 @@
+import {SubmitAJAXRequest} from "./modules/AJAX.js";
+
 $("#addToCart").submit(function(event) {
     event.preventDefault();
 
     let url = $(this).attr("action");
     let formData = new FormData(this);
 
-    $.ajax({
-        type: "POST",
-        url: url,
-        data: formData,
-        success: (returnData) =>{
-            alert("Added Item to Shopping Cart!");
-            let shoppingCart = document.querySelector(".cartTotal");
-            shoppingCart.innerText = returnData.ShoppingCartLength;  
-        },
-        datatype: "json",
-        processData: false,
-        contentType: false
-    })
+    let returnData = SubmitAJAXRequest(url, formData);
+    let shoppingCart = document.querySelector(".cartTotal");
+
+    shoppingCart.innerText = returnData.CartItems;
+})
+
+$(document).ready(()=>{
+    if(screen.width >= 450){
+        let zoomOptions = {
+            width: 500,
+            zoomWidth: 1000,
+            zoomContainer: document.querySelector(".zoomContainer"),
+            zoomStyle: "z-index: 2;position: absolute;"
+        };
+
+        new ImageZoom(document.querySelector(".zoomContainer"), zoomOptions);
+    }
 })
