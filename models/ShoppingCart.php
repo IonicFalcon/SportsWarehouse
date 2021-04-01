b67adee606bc480e61d7f984c715a33b7b6d7147
IonicFalcon <joshuajwallen@gmail.com>
2021-04-01 12:32:39 +1100

    Added Methods to Shopping Cart Class

--- models/ShoppingCart.php
+++ models/ShoppingCart.php
@@ -3,6 +3,11 @@
 class ShoppingCart{
     public $Items = [];
 
+    /**
+     * Calculate Total Price of all Items in Shopping Cart
+     *
+     * @return float
+     */
     public function CalculatePrice(){
         $price = 0;
 
@@ -15,5 +20,41 @@ class ShoppingCart{
         }
 
         return $price;
+    }
+
+    /**
+     * Return formatted size of Shopping Cart size
+     *
+     * @return string
+     */
+    public function ItemCount(){
+        $itemAmount = 0;
+
+        foreach($this->Items as $item){
+            $itemAmount += $item->Quantity;
+        }
+
+        if($itemAmount == 1){
+            return $itemAmount . " Item";
+        } else{
+            return $itemAmount . " Items";
+        }
+    }
+
+    /**
+     * Add Item to Shopping Cart. If Item already exists, add the quantities together
+     *
+     * @param Item $item
+     * @return void
+     */
+    public function AddItem($item){
+        foreach($this->Items as $shoppingCartItem){
+            if($item->ItemID === $shoppingCartItem){
+                $shoppingCartItem->Quantity += $item->Quantity;
+                return;
+            }
+        }
+
+        array_push($this->Items, $item);
     }
 }
