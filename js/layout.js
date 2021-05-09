55bd295dc71105910d5706acfc3959b72701ac6a
IonicFalcon <joshuajwallen@gmail.com>
2021-05-10 00:15:19 +1000

    Added admin popup functionality

--- js/layout.js
+++ js/layout.js
@@ -2,12 +2,21 @@ window.FontAwesomeConfig = {
     searchPseudoElements: true
 }
 
-function mobileMenu(hamburgerMenu){
+function mobileMenuToggle(){
     let mobileMenu = document.querySelector(".mobileNav");
-
-    if(hamburgerMenu.checked){
-        mobileMenu.classList.add("checked");
-    } else{
-        mobileMenu.classList.remove("checked");
-    }
+    mobileMenu.classList.toggle("checked");
 }
+
+$(".userOptions span.account").click(() => {
+    if(screen.width > 450){
+        let accountPopup = document.querySelector(".userOptions .accountPopup");
+        let overlay = document.querySelector(".overlay");
+        let body = document.querySelector(".root");
+
+        accountPopup.classList.toggle("open")
+        overlay.classList.toggle("modalOpen");
+        body.classList.toggle("modalOpen");
+    } else{
+        mobileMenuToggle();
+    }
+});
