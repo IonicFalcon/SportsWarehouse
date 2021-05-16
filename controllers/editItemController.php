028d88cbdb0a7116d236327b09e3c43bba435b0a
IonicFalcon <joshuajwallen@gmail.com>
2021-05-16 23:28:08 +1000

    Fixed XSS Vector

--- controllers/editItemController.php
+++ controllers/editItemController.php
@@ -2,6 +2,12 @@
 include "../models/Item.php";
 $items = Item::GetAllItems();
 
+foreach($items as $item){
+    $item->ItemName = htmlentities($item->ItemName);
+    $item->Description = htmlentities($item->Description);
+    $item->Category->CategoryName = htmlentities($item->Category->CategoryName);
+}
+
 if($_SERVER["REQUEST_METHOD"] == "GET"){
     echo '{"data":' . json_encode($items) . "}";
     die();
