534a9dd6c2a4b0586d2e5f3dc2e6c0f941dec99e
IonicFalcon <joshuajwallen@gmail.com>
2021-05-26 23:55:27 +1000

    Created Change Theme Page

---
+++ changeTheme.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once "models/Admin.php";
+$admin = Admin::RestrictPage();
+
+require_once "models/Category.php";
+$categories = Category::GetAllCategories();
+
+$pageTitle = "Change Theme - Sports Warehouse";
+
+ob_start();
+
+include "templates/changeTheme.html.php";
+
+$mainOutput = ob_get_clean();
+include "templates/layout.html.php";
---
+++ templates/changeTheme.html.php
@@ -0,0 +1,22 @@
+<section class="changeTheme">
+    <form id="adminThemeChange">
+        <p class="themeSelect" id="lightMode">
+            <input type="radio" name="theme">
+            <label for="lightMode">Light Theme</label>
+        </p>
+
+        <p class="themeSelect" id="darkMode">
+            <input type="radio" name="theme">
+            <label for="darkMode">Dark Theme</label>
+        </p>
+
+        <p class="themeSelect" id="custom">
+            <input type="radio" name="theme">
+            <label for="custom">Custom Theme</label>
+        </p>
+
+        <fieldset class="customTheme">
+            <p>Test</p>
+        </fieldset>
+    </form>
+</section>
